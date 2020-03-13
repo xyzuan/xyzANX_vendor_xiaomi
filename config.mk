8b26ed08782d35df23eddc931303a835e9e52ebf
Jody Yuantoro <xyzuan@qmail.id>
2020-03-13 07:43:42 +0700

    fix vendor_path

--- config.mk
+++ config.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VENDOR_PATH := vendor/xiaomi/MiuiCamera
+VENDOR_PATH := vendor/xiaomi/ANXCamera
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,system/etc)
 
