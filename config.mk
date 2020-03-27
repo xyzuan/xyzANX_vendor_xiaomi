13068ef9349899ea757e97ab9af09c6644667c2f
Jody Yuantoro <xyzuan@qmail.id>
2020-03-27 18:02:31 +0700

    rm lib

--- config.mk
+++ config.mk
@@ -18,7 +18,5 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib64,system/lib64)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib,system/lib)
-
 PRODUCT_PACKAGES += \
     ANXCamera
